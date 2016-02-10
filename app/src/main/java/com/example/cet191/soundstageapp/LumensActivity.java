9ebb6ed19256937de22b933ce9047f3a70fece20
pcloutier90 <pcloutie@emich.edu>
2016-02-10 12:37:09 -0500

    created lumens page

---
+++ app/src/main/java/com/example/cet191/soundstageapp/LumensActivity.java
@@ -0,0 +1,32 @@
+package com.example.cet191.soundstageapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+/*
+* Created by Phil on 2/9/2016
+*
+* */
+public class LumensActivity extends ActivityBaseClass {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lumens);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
