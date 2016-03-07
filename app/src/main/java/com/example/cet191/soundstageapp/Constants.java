de03abb07540c6a3c9bcf7b3f44e06a763b32153
Ashley <awoten@emich.edu>
2016-03-07 17:11:38 -0500

    testing, fingers crossed this actually works

--- app/src/main/java/com/example/cet191/soundstageapp/Constants.java
+++ app/src/main/java/com/example/cet191/soundstageapp/Constants.java
@@ -2,7 +2,6 @@ package com.example.cet191.soundstageapp;
 
 /**
  * Created by Eddie  on 3/5/2016.
- * bla
  */
 public interface Constants {
     String TAG = Thread.currentThread().getStackTrace()[1].getMethodName();
